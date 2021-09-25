cf6f77eada28d3fa4663f2bbf70a8087d9a5b670
Muhammad Ilham Mubarak <m.ilham.mubarak@gmail.com>
2021-09-25 23:37:47 +0700

    refactor Soal 1

--- src/dev/aang/TP/TP1/Soal1.java
+++ src/dev/aang/TP/TP1/Soal1.java
@@ -3,13 +3,13 @@ package dev.aang.TP.TP1;
 public class Soal1 {
 
     public static void main(String[] args) {
-        for(int i_1301194276 = 2; i_1301194276 < 11; i_1301194276++) {
-            System.out.printf("%d ", fibonnaci_1301194276(i_1301194276));
+        for(int i = 2; i < 11; i++) {
+            System.out.printf("%d ", fibonnaci(i));
         }
     }
     
-    private static int fibonnaci_1301194276(int x_1301194276) {
-        if (x_1301194276 < 2) return x_1301194276;
-        return fibonnaci_1301194276(x_1301194276 - 2) + fibonnaci_1301194276(x_1301194276 - 1);
+    private static int fibonnaci(int x) {
+        if (x < 2) return x;
+        return fibonnaci(x - 2) + fibonnaci(x - 1);
     }
 }
