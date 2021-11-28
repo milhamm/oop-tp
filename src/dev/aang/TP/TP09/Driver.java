87e99927e78ff01a89539fbb851d0857ee275e4e
Muhammad Ilham Mubarak <m.ilham.mubarak@gmail.com>
2021-11-28 19:04:14 +0700

    change value TP9

--- src/dev/aang/TP/TP09/Driver.java
+++ src/dev/aang/TP/TP09/Driver.java
@@ -6,21 +6,21 @@ import java.util.ArrayList;
 public class Driver {
 
     public static void main(String[] args) {
-        ArrayList<Side<String, Integer>> ar1 = new ArrayList<>();
-        ar1.add(new Side<>("Value", 60));
-        ar1.add(new Side<>("Year", 2021));
+        ArrayList<Side<String, Integer>> arr1 = new ArrayList<>();
+        arr1.add(new Side<>("Gluekol", 7200));
+        arr1.add(new Side<>("Cyanoacrylate", 1200));
 
         System.out.println("Array List 1");
-        for (Side<String, Integer> a: ar1) {
+        for (Side<String, Integer> a: arr1) {
             a.info();
         }
 
-        ArrayList<Side<LocalDate, Double>> ar2 = new ArrayList<>();
-        ar2.add(new Side<>(LocalDate.now(), 9.6));
-        ar2.add(new Side<>(LocalDate.now().minusDays(1), 7.8));
+        ArrayList<Side<LocalDate, Double>> arr2 = new ArrayList<>();
+        arr2.add(new Side<>(LocalDate.now(), 99.99));
+        arr2.add(new Side<>(LocalDate.now().minusDays(1), 66.66));
 
         System.out.println("Array List 2");
-        for (Side<LocalDate, Double> a: ar2) {
+        for (Side<LocalDate, Double> a: arr2) {
             a.info();
         }
     }
